c987745501efcb414ea50aa7447c7c9deec95345
yooncoderhere <jkim700345@gmail.com>
2025-02-15 15:00:35 +0900

    [Gold V] Title: 탑, Time: 316 ms, Memory: 123516 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -1,22 +1,22 @@
 import sys
-
-def solve():
-    N = int(sys.stdin.readline())  # 탑 개수
-    heights = list(map(int, sys.stdin.readline().split()))  # 탑 높이 리스트
-    result = [0] * N  # 결과 저장 (0으로 초기화)
-    stack = []  # 스택 (탑의 인덱스 저장)
-
-    for i in range(N):
-        while stack and heights[stack[-1]] < heights[i]:  # 현재 탑보다 낮은 탑 제거
+def solution(n,heights):
+    
+    stack = [] # 스택의 탑의 인덱스를 저장함
+    result = [0]*n
+    
+    for i in range(n):
+        while stack and heights[stack[-1]] < heights[i]:
+            #현재 탑보다 낮은 탑 제거
             stack.pop()
+            
+        if stack:
+            result[i] = stack[-1] +1
         
-        if stack:  # 스택이 비어있지 않다면, 가장 가까운 왼쪽 탑이 레이저를 수신
-            result[i] = stack[-1] + 1  # 1-based index 변환
+        stack.append(i)
+        
+    return " ".join(map(str,result))
+    
 
-        stack.append(i)  # 현재 탑 인덱스 스택에 저장
-
-    print(" ".join(map(str, result)))  # 결과 출력
-
-# 입력이 많을 수 있으므로 sys.stdin 사용
-if __name__ == "__main__":
-    solve()
+n= int(sys.stdin.readline())
+heights = list(map(int,sys.stdin.readline().split()))
+print(solution(n,heights))
