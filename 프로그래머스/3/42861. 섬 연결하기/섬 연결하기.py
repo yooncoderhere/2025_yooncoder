92d99e2924daa89bfaae25248878a4ee54146025
yooncoderhere <jkim700345@gmail.com>
2025-03-01 14:57:04 +0900

    [level 3] Title: 섬 연결하기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -9,30 +9,24 @@ def solution(n,costs):
     #비용을 기준으로 간선을 오름차순 정렬함
     costs.sort(key=lambda x: x[2])
     
-    #각 섬의 부모 노드를 저장함
-    parent = [i for i in range(n)]
+    parent = [i for i in range(n)] # 부모 노드를 저장
     
-    #루트 노드를 찾는 함수
+    #
     def find(node):
         if parent[node] != node:
-            parent[node] = find(parent[node]) # 재귀함수
+            parent[node] = find(parent[node])
         return parent[node]
     
-    # 두 노드를 합치는 함수
-    #초기 상태에서는 모든 노드가 자기 자신으로 가짐
-    #루트(집합을 대표하는 노드)가 다르면, 하나의 루트로 통일하여 연결함
     def union(node1,node2):
-        
         root1 = find(node1)
         root2 = find(node2)
         
         if root1 != root2:
             parent[root2] = root1
-    
-    #최소 비용 계산
+            
     answer = 0
-    for node1 , node2 , cost in costs:
-        if find(node1) != find(node2): # 두 노드가 다른 집합에 속해 있으면
-            union(node1,node2) # 두 노드를 연결
-            answer += cost # 비용을 추가함
+    for node1 , node2 ,cost in costs:
+        if find(node1) != find(node2):
+            union(node1,node2)
+            answer += cost
     return answer
